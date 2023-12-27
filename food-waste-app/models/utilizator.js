3fb5cb5caa002357bfa1a6eab4b68cc0775dc39b
ChowderChan <csgo_proAWP@yahoo.com>
2023-12-27 19:14:20 +0200

    Added DB

---
+++ food-waste-app/models/utilizator.js
@@ -0,0 +1,34 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../sequelize.js'
+
+const User = sequelize.define(
+  'User',
+  {
+    id: {
+
+        type: DataTypes.INTEGER,
+        primaryKey: true, 
+        autoIncrement: true
+    },
+    name:{
+
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password:{
+
+      type: DataTypes.STRING,
+      allowNull: false
+  },
+    phoneNumber: {
+
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+  },
+  {
+    tableName: 'users' 
+  }
+)
+
+export { User }
