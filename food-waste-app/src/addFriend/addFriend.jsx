a57a4cde2f1948d8aac5e28f173808418dcd8335
teabee53 <tiberiucristian2002@gmail.com>
2024-01-09 22:16:56 +0200

    Added friend list stuff to App.js and addFriend.jsx

--- food-waste-app/src/addFriend/addFriend.jsx
+++ food-waste-app/src/addFriend/addFriend.jsx
@@ -2,71 +2,67 @@ import React, { useState } from "react";
 import "./addFriend.css";
 import axios from "axios";
 
-const AddFriend = () => {
+const AddFriend = ({ onAddFriend }) => {
+  const [name, setName] = useState("");
 
-    const [name, setName] = useState("");
+  const options = [
+    { category: "Vegan" },
+    { category: "Vegetarian" },
+    { category: "Carnivorous" },
+  ];
 
-    const options = [
-        { category: "Vegan" },
-        { category: "Vegetarian" },
-        { category: "Carnivorous" },
-      ];
+  const [category, setCategory] = useState("Category");
+  const changeOption = (event) => {
+    setCategory(event.target.value);
+  };
 
-    const [category, setCategory] = useState("Category");
-    const changeOption = (event) => {
-        setCategory(event.target.value);
-      };
-    
-      const handleAddButtonClick = async () => {
+  const handleAddButtonClick = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/addFriend", {
+        name,
+        category,
+      });
 
-        try {
-          const response = await axios.post(
-            "http://localhost:5000/addFriend",
-            {
-                name,
-                category,
-            }
-          );
-    
-          console.log(response.data);
-        } catch (error) {
-          console.error("Action failed", error.message);
-        }
-      };
+      let newFriend = response.data.item;
+      onAddFriend(newFriend);
+    } catch (error) {
+      console.error("Action failed", error.message);
+    }
+  };
 
-      return (
-        <div className="containerFriendAdd">
-          <div className="headerFriendAdd">
-            <div className="textFriendAdd">Add friend in friend list</div>
-            <div className="underLineFriendAdd"></div>
-          </div>
-          <div className="inputsFriendAdd">
-            <div className="inputFriendAdd">
-              <input
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="inputFriendAdd">
-              <select value={category} onChange={changeOption}>
-                <option onChange={(e) => changeOption(e.target.value)}>
-                  Category
-                </option>
-                {options.map((ctr) => (
-                  <option value={ctr.category}>{ctr.category}</option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className="submitContainerFriendAdd">
-            <button className="add" onClick={handleAddButtonClick}>
-              Add
-            </button>
-          </div>
+  return (
+    <div className="containerFriendAdd">
+      <div className="headerFriendAdd">
+        <div className="textFriendAdd">Add friend in friend list</div>
+        <div className="underLineFriendAdd"></div>
+      </div>
+      <div className="inputsFriendAdd">
+        <div className="inputFriendAdd">
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-      );
+        <div className="inputFriendAdd">
+          <select value={category} onChange={changeOption}>
+            <option onChange={(e) => changeOption(e.target.value)}>
+              Category
+            </option>
+            {options.map((ctr) => (
+              <option value={ctr.category}>{ctr.category}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="submitContainerFriendAdd">
+        <button className="add" onClick={handleAddButtonClick}>
+          Add
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AddFriend;
