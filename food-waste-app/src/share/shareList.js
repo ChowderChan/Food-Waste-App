9892cd34cb46801993221a30ec1d17c8838046c1
ChowderChan <csgo_proAWP@yahoo.com>
2024-01-10 21:00:18 +0200

    Share on Facebook and Instagram begins part 2

--- food-waste-app/src/share/shareList.js
+++ food-waste-app/src/share/shareList.js
@@ -3,23 +3,22 @@ import { FacebookShareButton, InstagramShareButton } from 'react-share';
 
 const ShareList = ({ username, shareableItems }) => {
   const generateCustomShareContent = () => {
-    // Customize the shareable content based on your requirements
     return shareableItems.map((item) => `${item.name} - ${item.category}`).join('\n');
   };
 
   return (
     <div>
-      {/* Facebook Share Button for the custom shareable content */}
+      
       <FacebookShareButton
-        url={`https://your-app-url.com/login-signup`} // Replace with your login/signup page URL
+        url={`localhost:3000`}
         quote={`Check out ${username}'s Shareable Items List: \n${generateCustomShareContent()}`}
       >
         Share on Facebook
       </FacebookShareButton>
 
-      {/* Instagram Share Button for the custom shareable content */}
+      
       <InstagramShareButton
-        url={`localhost:3000`} // Replace with your login/signup page URL
+        url={`localhost:3000`}
         title={`Check out ${username}'s Shareable Items List: \n${generateCustomShareContent()}`}
       >
         Share on Instagram
